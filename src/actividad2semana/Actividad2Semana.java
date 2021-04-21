af15162c0c641d02635ccaa63451120245a4f1b2
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-04-20 23:20:32 -0500

    Segundo ejercicio - Actividad 2

---
+++ src/actividad2semana/Actividad2Semana.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad2semana;
+import java.util.Scanner;
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class Actividad2Semana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Segundo ejercicio");
+        // CREAR UN MÉTODO QUE DETERMINE SI EL DÍA ES HÁBIL (LUNES A VIERNES)Y EMITA UN MENSAJE SI ES FIN DE SEMANA.
+    
+    int dia = 0;
+    
+    Scanner miConsola = new Scanner(System.in);
+    
+      
+    System.out.println("Introduce un día de la semana en base a:\n Lunes = 1 \n Martes = 2 \n Miércoles = 3 \n Jueves = 4 \n Viernes = 5 \n Sabado = 6 \n Domingo = 7");
+    dia = miConsola.nextInt();
+    
+    if (dia == 1) {
+        System.out.println("Escribiste Lunes, es un día hábil");
+    } else {
+    if (dia == 2) {
+        System.out.println("Escribiste Martes, es un día hábil");
+    } else {
+    if (dia == 3) {
+        System.out.println("Escribiste Miércoles, es un día hábil");
+    } else {
+    if (dia == 4) {
+        System.out.println("Escribiste Jueves, es un día hábil");
+    } else {
+    if (dia == 5) {
+        System.out.println("Escribiste Viernes, es un día hábil");
+    } else {
+    if (dia == 6) {
+        System.out.println("Escribiste Sábado, es fin de semana");
+    } else {
+    if (dia == 7) {
+        System.out.println("Escribiste Domingo, es fin de semana");
+    } else {
+        System.out.println("Lo siento, ingresaste un dato incorrecto");
+    }   
+    }    
+    }
+    }  
+    } 
+    } 
+    }
+    
+
+
+    
+    }
+    
+}
